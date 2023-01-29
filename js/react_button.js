87769b02d1a5059adf9416714bf96fc48d7305a3
RostyslavOliinyk <123735702+RostyslavOliinyk@users.noreply.github.com>
2023-01-29 05:13:09 +0200

    Add React button to website

--- js/react_button.js
+++ js/react_button.js
@@ -1,1 +1,9 @@
+'use strict';
 
+ReactDOM.render(
+    React.createElement('button', 
+        {className: 'promo__btn', 
+        onClick: function() { alert("Oops, I must have forgotten to upload it :)"); }
+    }, 'Download Resume'), 
+    document.getElementById('react_button')
+);
